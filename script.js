e094ffd0f84fb6efbeb49e33a8ddab6e6dc28147
TUSHAR-DALVI-09 <tushardalvi099@gmail.com>
2024-12-06 12:06:18 +0530

    Add both Code

---
+++ script.js
@@ -0,0 +1,21 @@
+let a=document.getElementById("img");
+
+function myFunction(){
+    a.style.animationDuration = 3+"s";
+}
+
+function myFunoff(){
+    a.style.animationDuration = 0+"s";
+}
+
+function myFun2(){
+    a.style.animationDuration = 1+"s";
+}
+
+function myFun3() {
+    a.style.animationDuration = 0.5+"s";
+}
+
+function myFun4(){
+    a.style.animationDuration=0.1+"s"
+}
